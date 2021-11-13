0d5f385beb035d84cc05940d9613fb9c7281156a
Mulder Sidney <Sidney.Mulder@han.nl>
2021-11-13 21:34:51 +0100

    verwijzing username verwijderd

--- login/register.php
+++ login/register.php
@@ -2,12 +2,12 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/db_connect.php';
 
 // Now we check if the data was submitted, isset() function will check if the data exists.
-if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+if (!isset($_POST['password'], $_POST['email'])) {
     // Could not get the data that should have been sent.
     exit('Please complete the registration form!');
 }
 // Make sure the submitted registration values are not empty.
-if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+if (empty($_POST['password']) || empty($_POST['email'])) {
     // One or more values are empty.
     exit('Please complete the registration form');
 }
@@ -16,18 +16,14 @@ if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
     exit('Email is not valid!');
 }
 
-if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
-    exit('Username is not valid!');
-}
-
 if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
     exit('Password must be between 5 and 20 characters long!');
 }
 
 // We need to check if the account with that username exists.
-if ($stmt = $mysqli->prepare('SELECT id, password FROM personen WHERE username = ?')) {
+if ($stmt = $mysqli->prepare('SELECT id, password FROM personen WHERE email = ?')) {
     // Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
-    $stmt->bind_param('s', $_POST['username']);
+    $stmt->bind_param('s', $_POST['email']);
     $stmt->execute();
     $stmt->store_result();
     // Store the result so we can check if the account exists in the database.
@@ -36,10 +32,9 @@ if ($stmt = $mysqli->prepare('SELECT id, password FROM personen WHERE username =
         echo 'Username exists, please choose another!';
     } else {
         // Username doesnt exists, insert new account
-        if ($stmt = $mysqli->prepare('INSERT INTO personen (password, email, activation_code) VALUES (?, ?, ?, ?)')) {
+        if ($stmt = $mysqli->prepare('INSERT INTO personen (password, email, activation_code) VALUES (?, ?, ?)')) {
             // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-//            $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
             $uniqid = uniqid();
             $stmt->bind_param('sss', $password, $_POST['email'], $uniqid);
             $stmt->execute();
